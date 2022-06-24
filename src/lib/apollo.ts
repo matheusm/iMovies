d35d2c202cd01b0a3eb5b52018997180f635ee47
Matheus Martins <matheusms280100@gmail.com>
2022-06-24 11:18:18 -0300

    feat(lesson) add base list to sidebar

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,7 +1,6 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
-
 export const client = new ApolloClient({
-  uri: import.meta.env.APOLO_API_KEY,
+  uri: import.meta.env.VITE_APP_APOLO_API_KEY,
   cache: new InMemoryCache()
 })
