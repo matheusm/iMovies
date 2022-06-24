eeee518b278d4951e3d7f56d7b893786f865b33b
Matheus Martins <matheusms280100@gmail.com>
2022-06-24 09:56:40 -0300

    fix(enviroment) change to vite pattern

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,6 +1,7 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
+
 export const client = new ApolloClient({
-  uri: process.env.APOLO_API_KEY,
+  uri: import.meta.env.APOLO_API_KEY,
   cache: new InMemoryCache()
 })
